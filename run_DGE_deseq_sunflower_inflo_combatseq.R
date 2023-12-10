4d67585d67c229fd44fe63658833d3c6bb65adb2
emily yaklich <44010226+emilyyaklich@users.noreply.github.com>
2023-12-10 14:01:00 -0500

    Update run_DGE_deseq_sunflower_inflo_combatseq.R

--- run_DGE_deseq_sunflower_inflo_combatseq.R
+++ run_DGE_deseq_sunflower_inflo_combatseq.R
@@ -32,7 +32,7 @@ metadata<-data.frame(samples, dev_stage)
 # create the factors of interest
 metadata$dev_stage<-factor(metadata$dev_stage)
 
-# create the model
+# create the model (wrt dev_stage)
 summed_counts<-DESeqDataSetFromMatrix(counts(summed_counts),colData = metadata, design=~0+dev_stage)
 
 # pre-filter for reads where at least 3 samples have a count of 1 or higher
@@ -43,14 +43,17 @@ summed_counts_filt<-summed_counts[keep,]
 # get a dataframe of the counts
 count_matrix <- as.matrix(counts(summed_counts_filt))
 
+# sort by batch (sample group) and group (dev_stage)
 batch <- c(1,1,1,1,2,3,2,3,2,3,2,3)
 group <- c(1,2,3,4,1,1,2,2,3,3,4,4)
 
+# adjust counds using combat seq...output is a matrix of adjusted counts 
 adjusted_counts <- ComBat_seq(count_matrix, batch=batch, group=group)
 
+# write to a CSV file
 write.csv(as.data.frame((adjusted_counts)), file='deseq_results/adjusted_counts_combatseq.csv')
 
-# plot MDS of adjusted counts
+# plot MDS of adjusted counts...can compare this with previous plot pre-combat seq
 glimmaMDS(adjusted_counts, group=metadata)
 
 # plot PCA of adjusted counts
@@ -60,68 +63,21 @@ plotPCA(adjusted_counts,labels=TRUE, col=dev_stage)
 legend("topright",inset=c(-0.4,0),legend=unique(dev_stage), fill=dev_stage)
 dev.off()
 
-# create the model
+# create the model with adjusted counts
 adjusted_counts_deseq<-DESeqDataSetFromMatrix((adjusted_counts),colData = metadata, design=~0+dev_stage)
-
 
 # run the DGE analysis
 DESeq_dataset_results_combatseq<-DESeq(adjusted_counts_deseq,parallel=TRUE)
+# save the output
 saveRDS(DESeq_dataset_results_combatseq, file='deseq_results/combatseq/pairwise/deseq_dataset_results_combatseq.RData')
 
-
+# set up the pairwise contrasts (10v20, 20v30, 30v35)
 result_10D_v_20D_combatseq<-results(DESeq_dataset_results_combatseq,contrast=c("dev_stage","10D","20D"),alpha=0.05,parallel=TRUE)
 result_20D_v_30D_combatseq<-results(DESeq_dataset_results_combatseq,contrast=c("dev_stage","20D","30D"),alpha=0.05,parallel=TRUE)
 result_30D_v_35D_combatseq<-results(DESeq_dataset_results_combatseq,contrast=c("dev_stage","30D","35D"),alpha=0.05,parallel=TRUE)
 
-
+# write to CSV file
 write.csv(as.data.frame(result_10D_v_20D_combatseq), file='deseq_results/combatseq/pairwise/result_10D_v_20D.csv')
 write.csv(as.data.frame(result_20D_v_30D_combatseq), file='deseq_results/combatseq/pairwise/result_20D_v_30D.csv')
 write.csv(as.data.frame(result_30D_v_35D_combatseq), file='deseq_results/combatseq/pairwise/result_30D_v_35D.csv')
-
-
-
-
-# try in edgeR
-
-
-design<-model.matrix(~0+dev_stage, data=metadata)
-y<- DGEList(counts=(adjusted_counts), group=dev_stage)
-y<-calcNormFactors(y,method="TMM")
-
-# estimate dispersion
-y <-estimateGLMCommonDisp(y, design)
-y <-estimateGLMTagwiseDisp(y,design)
-plotBCV(y)
-
-# fit the model
-fit<-glmQLFit(y,design)
-
-# use the output from the line below to design the contrasts 
-colnames(fit)
-
-
-res_10v20<-glmQLFTest(fit, contrast=c(-1,1,0,0))
-summary(decideTests(res_10v20))
-plotMD(res_10v20)
-top_10v20<-topTags(res_10v20, n=Inf)
-write.csv(as.data.frame(top_10v20), file='deseq_results/combatseq/lrt/result_10D_v_20D.csv')
-
-
-
-res_20v30<-glmQLFTest(fit, contrast=c(0,-1,1,0))
-summary(decideTests(res_20v30))
-plotMD(res_20v30)
-top_20v30<-topTags(res_20v30, n=Inf)
-write.csv(as.data.frame(top_20v30), file='deseq_results/combatseq/lrt/result_20D_v_30D.csv')
-
-
-
-
-res_30v35<-glmQLFTest(fit, contrast=c(0,0,-1,1))
-summary(decideTests(res_30v35))
-plotMD(res_30v35)
-top_30v35<-topTags(res_30v35, n=Inf)
-write.csv(as.data.frame(top_30v35), file='deseq_results/combatseq/lrt/result_30D_v_35D.csv')
-
-
 
